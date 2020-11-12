78927d333b1b5345ba09838e3b99b9608541ee74
vasi <vasihh404@gmail.com>
2020-11-12 22:37:09 +0530

    add import 'package:http/http.dart' as http; for more understandable http.Response http.get

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,7 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async{
-    Response response = await get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0ed13d359f543f1068186b4b6c0c7ad1');
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0ed13d359f543f1068186b4b6c0c7ad1');
     print(response.statusCode);
     print(response.body);
     print(response.hashCode);
